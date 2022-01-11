3231a2d4926f8269ac736b2c44699c42e78ef6fc
BFSmx <92232284+BFSmx@users.noreply.github.com>
2022-01-11 11:20:35 -0600

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,7 +8,7 @@ export default class HomeScreen extends Component{
       text: '',
       isSearchPressed: false,
       isLoading: false,
-      word  : "Loading...",
+      word  : "Cargando...",
       lexicalCategory :'',
       definition : ""
     };
@@ -68,7 +68,7 @@ export default class HomeScreen extends Component{
         <Header
           backgroundColor={'purple'}
           centerComponent={{
-            text: 'Pocket Dictionary',
+            text: 'Diccionario de Bolsillo',
             style: { color: '#fff', fontSize: 20 },
           }}
         />
@@ -80,7 +80,7 @@ export default class HomeScreen extends Component{
               this.setState({
                 text: text,
                 isSearchPressed: false,
-                word  : "Loading...",
+                word  : "Cargando...",
                 lexicalCategory :'',
                 examples : [],
                 definition : ""
@@ -101,13 +101,13 @@ export default class HomeScreen extends Component{
         <View style={styles.outputContainer}>
           <Text style={{fontSize:20}}>
             {
-              this.state.isSearchPressed && this.state.word === "Loading..."
+              this.state.isSearchPressed && this.state.word === "Cargando..."
               ? this.state.word
               : ""
             }
           </Text>
             {
-              this.state.word !== "Loading..." ?
+              this.state.word !== "Cargando..." ?
               (
                 <View style={{justifyContent:'center', marginLeft:10 }}>
                   <View style={styles.detailsContainer}>
